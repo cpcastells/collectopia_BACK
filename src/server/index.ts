c04fe756882fb59dbfe7d5578448740717277fcf
Carles Pueyo <cpcastells@gmail.com>
2023-05-25 12:24:13 +0200

    Set CORS middleware with allowed origin

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,16 @@
 import express from "express";
+import cors from "cors";
+
+const allowedOrigin = process.env.ALLOWED_ORIGIN;
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigin,
+};
 
 const app = express();
+
+app.use(cors(options));
+
+app.disable("x-powered-by");
 
 export default app;
