fb35a8be209e6aa3fad9fbff9d5304e17ad826cb
Carles Pueyo <cpcastells@gmail.com>
2023-05-25 15:36:25 +0200

    Create notFoundError middleware

--- src/server/middlewares/errors/errorMiddlewares.ts
+++ src/server/middlewares/errors/errorMiddlewares.ts
@@ -1,10 +1,20 @@
 import "../../../loadEnvironment.js";
 import { type NextFunction, type Request, type Response } from "express";
 import createDebug from "debug";
-import type CustomError from "../../CustomError/CustomError.js";
+import CustomError from "../../CustomError/CustomError.js";
 import chalk from "chalk";
 
 const debug = createDebug("collectopia-api:root:server:middlewares:errors");
+
+export const notFoundError = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const error = new CustomError(404, "Endpoint not foung");
+
+  next(error);
+};
 
 export const generalError = (
   error: CustomError,
