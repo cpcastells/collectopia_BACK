34bfe674445e821acd474b8fc201f7a3b849e2cf
Carles Pueyo <cpcastells@gmail.com>
2023-05-25 15:37:26 +0200

    Add notFoundError middleware to index server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import { generalError } from "./middlewares/errors/errorMiddlewares.js";
+import {
+  generalError,
+  notFoundError,
+} from "./middlewares/errors/errorMiddlewares.js";
 
 const allowedOrigin = process.env.ALLOWED_ORIGIN;
 
@@ -16,6 +19,8 @@ app.use(cors(options));
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
+
+app.use(notFoundError);
 
 app.use(generalError);
 
