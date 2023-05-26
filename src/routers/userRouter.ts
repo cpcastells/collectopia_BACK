f9a78c8f80bbf2d47c9546aa5e9a6e9fb4f3081b
Carles Pueyo <cpcastells@gmail.com>
2023-05-26 11:26:51 +0200

    Create userRouter and add validation to endpoint POST 'user/login'

---
+++ src/routers/userRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import loginUser from "../server/controllers/userControllers/userControllers.js";
+import { validate } from "express-validation";
+import loginSchema from "../schemas/loginSchema/loginSchema.js";
+
+const userRouter = Router();
+
+userRouter.post(
+  "/login",
+  validate(loginSchema, {}, { abortEarly: false }),
+  loginUser
+);
+
+export default userRouter;
