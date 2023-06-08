d38a21ce03ca8cff70af1f0f57ca4dbc20c8ebed
Carles Pueyo <cpcastells@gmail.com>
2023-06-08 23:56:25 +0200

    Add createBoardgame controller

--- src/server/controllers/boardgamesControllers/boardgamesControllers.ts
+++ src/server/controllers/boardgamesControllers/boardgamesControllers.ts
@@ -1,6 +1,8 @@
 import { type NextFunction, type Response } from "express";
 import Boardgame from "../../../database/models/Boardgame/Boardgame.js";
 import { type CustomRequest } from "../../../types.js";
+import { Types } from "mongoose";
+import CustomError from "../../CustomError/CustomError.js";
 
 export const getBoardgames = async (
   req: CustomRequest,
@@ -33,6 +35,35 @@ export const deleteBoardgame = async (
     }
 
     res.status(200).json({ message: "Boardgame deleted!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createBoardgame = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userBoardgame = req.body;
+    const { userId } = req;
+
+    const newBoardgame = await Boardgame.create({
+      ...userBoardgame,
+      user: new Types.ObjectId(userId),
+    });
+
+    if (!newBoardgame) {
+      const customError = new CustomError(
+        404,
+        "Error: no boardgame was created"
+      );
+
+      throw customError;
+    }
+
+    res.status(201).json({ boardgame: newBoardgame });
   } catch (error) {
     next(error);
   }
